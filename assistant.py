aa3b4b5303efc1401a308189101dcc0e2da3a5f2
arielm7 <arielmv07@gmail.com>
2018-07-22 00:47:24 -0600

    se agrega la funcionalidad de palabra clave y el comienzo de la logica de asistente

---
+++ assistant.py
@@ -0,0 +1,23 @@
+import keyWordListener
+import speechToText
+import speech_recognition as sr
+from playsound import playsound
+
+if __name__ == "__main__":
+    r=sr.Recognizer()
+    m=sr.Microphone()
+    while(True):
+	    keyWordListener.keyWordListener()
+	    response=speechToText.speechRecognizer(r,m)
+	    if response["success"]==True:
+	    	playsound('/home/ariel/LevelUp/success.mp3')
+	    	speechToText.playVoice('realizando orden')
+	    	##send trascription to server
+	    	##KNX
+	    	##
+
+	    else:
+	    	playsound('/home/ariel/LevelUp/fail.mp3')
+	    	speechToText.playVoice(response["errorCode"])
+
+    
